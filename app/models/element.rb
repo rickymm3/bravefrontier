d2a5a67556bafc5644cf32b526561fa74a133e37
Rick Mason <rickymm3@gmail.com>
2014-04-01 21:03:18 -0400

    fixed navbar issue

--- app/models/element.rb
+++ app/models/element.rb
@@ -2,7 +2,7 @@ class Element < ActiveRecord::Base
 
   has_many :units
 
-  def self.get_element_string (element_id)
+  def self.get_element_class (element_id)
     element = Element.find(element_id)
     'elements-'+element.name.to_s
   end
