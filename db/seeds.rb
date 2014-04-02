2a9e07e4f79623c1dd01f45638e92f7ff0382122
Rick Mason <rickymm3@gmail.com>
2014-04-01 23:49:47 -0400

    removed users from seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,9 +10,6 @@ Element.create!([
   {name: "earth"},
   {name: "multi"}
 ])
-Profile.create!([
-  {username: "rickymm3", description: nil, user_id: 1}
-])
 Unit.create!([
   {name: "Fencer Vargas", description: "A swordsman from the ruined Agni Empire, he later became one of the Six Heroes. Proud of his late father, a Knight in the Order of Agni, he trained in the long sword, becoming a formidable knight. Although he is well-known for his countless victories over other warriors, he still considers himself inexperienced and continues to train with great zeal.", no: 1, element_id: 1, rarity_id: 2, base_hp: nil, base_atk: nil, base_def: nil, base_rec: nil, ls_id: nil, bb_id: nil, numhits: 3, slug: "fencer-vargas", realname: nil},
   {name: "Scythe Alice", description: "", no: 170, element_id: 4, rarity_id: 4, base_hp: nil, base_atk: nil, base_def: nil, base_rec: nil, ls_id: nil, bb_id: nil, numhits: nil, slug: "scythe-alice", realname: nil}
