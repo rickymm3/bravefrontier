b332920143fb09257b2d03f7a03605e1c39510fc
Rick Mason <rickymm3@gmail.com>
2014-04-17 21:08:57 -0400

    seed change and slight unit model edit

--- app/models/unit.rb
+++ app/models/unit.rb
@@ -5,7 +5,7 @@ class Unit < ActiveRecord::Base
 
   has_one :element
 
-  validates :name, presence: true, uniqueness: {case_sensitive: false}
+  # validates :name, presence: true, uniqueness: {case_sensitive: false}
 
   #has_attached_file :fullpic, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "assets/missing.png"
   #validates_attachment_content_type :fullpic, :content_type => /\Aimage\/.*\Z/
